3833e7dcde983b9e4c25068f5d69e7cbe07eeebb
JenningsYeUT <105323649+JenningsYeUT@users.noreply.github.com>
2022-05-17 15:46:33 -0500

    Add files via upload

---
+++ Code/Comparison_Plotter.m
@@ -0,0 +1,25 @@
+% load saved figures
+fig1   = hgload('AnnmaxGumbel.fig');
+fig2   = hgload('AnnmaxGEV.fig');
+%fig3   = hgload('AnnmaxWeibull.fig');
+%  Prepare 'subplot'
+figure
+h1=subplot(1,1,1);
+h2=subplot(1,1,1);
+%h3=subplot(1,1,1);
+line1=copyobj(allchild(get(fig1,'CurrentAxes')),h1);
+set(line1, 'color', 'blue');
+hold on
+line2=copyobj(allchild(get(fig2,'CurrentAxes')),h1);
+set(line2, 'color', 'r');
+%line3=copyobj(allchild(get(fig3,'CurrentAxes')),h1);
+%set(line3, 'color', 'g');
+xlabel('Peaks over Threshold Hs [m]')
+ylabel('Exceedance probability')
+set(gca,'yscale','log')
+set(gca,'YMinorTick','off')
+set(gca,'YMinorGrid','off')
+set(gca,'YTick',[1e-2 2e-2 1e-1 2e-1 1])
+set(gca,'YTickLabel',{'1/100','1/50','1/10','1/5','1'})
+title(['GEV and Gumbel'])
+legend('Gumbel', '','','','GEV')
---
+++ Code/baseline_analysis_PoT_Gumbel.m
@@ -0,0 +1,99 @@
+clear
+
+% select case
+exercise=1;
+site=1;
+
+% load data
+filename=['Exercise' num2str(exercise) '_Site' num2str(site) '_FIOESM_data.csv'];
+data=dlmread(filename,',',1,0);
+year=data(:,1);
+month=data(:,2);
+Hs=data(:,5);
+
+% create subset with PoT
+thprc=99.9;
+threshold=prctile(Hs, thprc);
+pks=findpeaks(Hs);
+%pks=findpeaks(Hs,'MinPeakProminence',range(Hs)*.10);
+%pks=findpeaks(Hs,'MinPeakDistance',1440);
+indices=find(pks>threshold);
+peaks=pks(indices);
+
+% fit gumbel distribution
+[mu, sigma]=gumbel_fit_mom(peaks);
+
+% calculate quantiles
+N=logspace(0,3,length(peaks));
+P=1-1./N;
+x=gumbel_inv(P,mu,sigma);
+
+% calculate return values
+T=[5 50 500];
+RV=gumbel_inv(1-1./T,mu,sigma);
+
+% bootstrap record to get confidence interval
+trials=1000;
+x_boot=zeros(trials,length(P));
+RV_boot=zeros(trials,3);
+for j=1:trials
+    z=resample(peaks);
+    [mu, sigma]=gumbel_fit_mom(z);
+    x_boot(j,:)=gumbel_inv(P,mu,sigma);
+    RV_boot(j,:)=gumbel_inv(1-1./T,mu,sigma);
+end
+x_boot=sort(x_boot);
+RV_boot=sort(RV_boot);
+
+% write return value outputs
+if exercise==1
+    filename=['Ex' num2str(exercise) '_Site' num2str(site) '_ReturnValues_Baseline.csv'];
+    header='Return Period [years],Return Value [m],CI 2.5%% [m],CI 97.5%% [m]\n';
+elseif exercise==2
+    filename=['Ex' num2str(exercise) '_Site' num2str(site) '_Quantiles_Baseline.csv'];
+    header='Exceedance Prob,Quantile [m],CI 2.5%% [m],CI 97.5%% [m]\n';
+end
+fid=fopen(filename,'w');
+fprintf(fid,header);
+fclose(fid);
+RVlow=RV_boot(round(trials*0.025),:);
+RVhigh=RV_boot(round(trials*0.975),:);
+dlmwrite(filename,[T(:) RV(:) RVlow(:) RVhigh(:)],'-append');
+
+% write distribution outputs
+if exercise==1
+    filename=['Ex' num2str(exercise) '_Site' num2str(site) '_AnnualDist_Baseline.csv'];
+elseif exercise==2
+    filename=['Ex' num2str(exercise) '_Site' num2str(site) '_F25Dist_Baseline.csv'];
+end
+header='Exceedance Prob,Quantile [m],CI 2.5%% [m],CI 97.5%% [m]\n';
+fid=fopen(filename,'w');
+fprintf(fid,header);
+fclose(fid);
+xlow=x_boot(round(trials*0.025),:);
+xhigh=x_boot(round(trials*0.975),:);
+dlmwrite(filename,[1-P(:) x(:) xlow(:) xhigh(:)],'-append');
+
+% calculate empirical exceedance probabilities
+n=length(peaks);
+k=1:n;
+Pempirical=k/(n+1);
+
+% plot results
+fig=figure;
+hold on; box on; grid on
+plot(sort(peaks),1-Pempirical,'ko')
+plot(x,1-P,'r')
+plot(xlow,1-P,'b--')
+plot(xhigh,1-P,'b--')
+xlabel('Peaks over Threshold Hs [m]')
+ylabel('Exceedance probability')
+title(['PoT' char(8211) 'Threshold = ' num2str(thprc) '%, Gumbel'])
+annotation('textbox','String',append('n = ', num2str(n)),'FitBoxToText','on');
+ylim([1e-2 1])
+set(gca,'yscale','log')
+set(gca,'YMinorTick','off')
+set(gca,'YMinorGrid','off')
+set(gca,'YTick',[1e-2 2e-2 1e-1 2e-1 1])
+set(gca,'YTickLabel',{'1/100','1/50','1/10','1/5','1'})
+saveas(fig, 'GumbelDistance1440Threshold999.fig')
